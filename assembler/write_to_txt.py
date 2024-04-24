919a5880c6795fbfeb8b18f3da69a98dfdf0efe2
Zach Riane Machacon <zachriane01@gmail.com>
2024-04-24 18:59:19 +0800

    chore: added docstring for write_to_txt

--- assembler/write_to_txt.py
+++ assembler/write_to_txt.py
@@ -4,6 +4,22 @@ OUTPUT_PATH = "./output"
 
 
 def write_to_txt(original_file_name: str, assembled_lines: list) -> None:
+    """
+    Writes the provided list of lines to a text file with a modified file name.
+
+    This function creates a text file named `{original_file_name}_assembled.txt`
+    in a predetermined output directory. Each line from the list `assembled_lines`
+    is written to this file, followed by a newline character.
+
+    Parameters:
+        original_file_name (str): The base name of the file to which the lines
+                                  will be written, without the file extension.
+        assembled_lines (list): A list of strings, each representing a line to
+                                be written to the file.
+
+    Returns:
+        None
+    """
     with open(
         os.path.join(OUTPUT_PATH, f"{original_file_name}_assembled.txt"), "w"
     ) as current_txt_file:
